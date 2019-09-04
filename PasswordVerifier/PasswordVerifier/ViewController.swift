bb8ef7e6b98f777ca037e530fd92e6e9ffb4887a
Treinamento <treinamento@lcl-adms-iMac-4.local>
2019-09-04 16:00:59 -0300

    First commit

---
+++ PasswordVerifier/PasswordVerifier/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PasswordVerifier
+//
+//  Created by Treinamento on 8/28/19.
+//  Copyright © 2019 In.kpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var user: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBAction func signIn(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
